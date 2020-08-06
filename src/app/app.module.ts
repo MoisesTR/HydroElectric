f965e23ee3ad4dee319e1cb469e1f0c65feb19c9
JackZeled0n <jackiezeledon@gmail.com>
2020-08-05 21:27:29 -0600

    Add menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {MDBBootstrapModulesPro, MDBSpinningPreloader} from 'ng-uikit-pro-standard';
+import { MenuComponent } from './pages/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModulesPro.forRoot()
+    MDBBootstrapModulesPro.forRoot(),
+    RouterModule
   ],
   providers: [MDBSpinningPreloader],
   bootstrap: [AppComponent]
